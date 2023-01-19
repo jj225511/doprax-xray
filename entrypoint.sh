9074246d164401a86351e4140b8c4b56c6cb68cc
jj225511 <admin@zyzh.cc>
2023-01-19 22:14:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 apt update && apt install -y supervisor wget unzip iproute2
 # wget -qO- https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep -o '"https://github.com/XTLS/Xray-core/releases/download/.*/Xray-linux-64.zip"' | xargs wget -O m.zip
 wget -O m.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
@@ -8,6 +8,10 @@ sed -i "s/uuid/$uuid/g" ./config.yaml
 sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
 [ -n "${www}" ] && rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/yonggekkk/doprax-xray/raw/main/3w/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
 xpid=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+wget -O x-ui-linux-amd64.tar.gz https://github.com/FranzKafkaYu/x-ui/releases/latest/download/x-ui-linux-amd64.tar.gz
+tar -zxvf x-ui-linux-amd64.tar.gz
+chmod +x ./x-ui/x-ui
+./x-ui/x-ui
 mv xray $xpid
 cat config.yaml | base64 > config
 rm -f config.yaml
